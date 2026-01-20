0bd11b64859def102349307a00f8990f984e9eb0
Manpreet Singh <manpreets95828@gmail.com>
2026-01-20 23:41:56 +0530

    Database setup using convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+"use client";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const tasks = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
   return (
-    <div><Button >
-     Click Me
-    </Button></div>
-    
-  );
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {tasks?.map(({ _id, name }) => <div key={_id}>{name}</div>)}
+    </main>
+  ); 
 }
